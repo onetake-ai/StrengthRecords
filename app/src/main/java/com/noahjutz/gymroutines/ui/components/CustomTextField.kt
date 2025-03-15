1e4c45db9dcb7e27e1457dff91871c477b4ec7d7
Noah Jutz <noah@noahjutz.com>
2025-03-15 18:42:01 +0100

    Use material3 in CustomTextField

--- app/src/main/java/com/noahjutz/gymroutines/ui/components/CustomTextField.kt
+++ app/src/main/java/com/noahjutz/gymroutines/ui/components/CustomTextField.kt
@@ -20,9 +20,13 @@ package com.noahjutz.gymroutines.ui.components
 
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.LocalContentColor
-import androidx.compose.material.LocalTextStyle
-import androidx.compose.runtime.*
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
