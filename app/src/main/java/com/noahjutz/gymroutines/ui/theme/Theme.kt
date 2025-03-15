5b7bd1600b30400e010b9bf4c71f2047fc81d26a
Noah Jutz <noah@noahjutz.com>
2025-03-15 20:06:23 +0100

    Use material3 in Theme
    Removed custom theme colors for now

--- app/src/main/java/com/noahjutz/gymroutines/ui/theme/Theme.kt
+++ app/src/main/java/com/noahjutz/gymroutines/ui/theme/Theme.kt
@@ -1,30 +1,9 @@
 package com.noahjutz.gymroutines.ui.theme
 
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-
-val WhiteColorPalette = lightColors(
-    primary = Primary,
-    primaryVariant = PrimaryDark,
-    secondary = Secondary,
-    secondaryVariant = SecondaryDark,
-    onPrimary = Color.White,
-    onSecondary = Color.Black
-)
-
-val BlackColorPalette = darkColors(
-    primary = PrimaryDesaturated,
-    primaryVariant = PrimaryDark,
-    secondary = Secondary,
-    secondaryVariant = Secondary,
-    onPrimary = Color.Black,
-    onSecondary = Color.Black
-)
 
 @Composable
 fun GymRoutinesTheme(
