7582b01c6c881377becd2d77904faa3f9329c342
Noah Jutz <noah@noahjutz.com>
2025-03-15 20:01:25 +0100

    Use material3 in RoutineListPlaceholder

--- app/src/main/java/com/noahjutz/gymroutines/ui/routines/list/RoutineListPlaceholder.kt
+++ app/src/main/java/com/noahjutz/gymroutines/ui/routines/list/RoutineListPlaceholder.kt
@@ -6,16 +6,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ListItem
-import androidx.compose.material.Text
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.placeholder.material.placeholder
 
-@ExperimentalMaterialApi
 @Composable
 fun RoutineListPlaceholder() {
     Column {
@@ -28,13 +26,11 @@ fun RoutineListPlaceholder() {
                 .placeholder(visible = true)
         )
         repeat(3) {
-            ListItem {
+            ListItem(headlineContent = {
                 Text(
-                    "A".repeat((5..15).random()),
-                    modifier = Modifier
-                        .placeholder(visible = true)
+                    "A".repeat((5..15).random()), modifier = Modifier.placeholder(visible = true)
                 )
-            }
+            })
         }
     }
 }
