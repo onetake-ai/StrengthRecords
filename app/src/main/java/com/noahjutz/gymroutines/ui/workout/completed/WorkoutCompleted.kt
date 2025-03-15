11d85c84eb70f653a216d2bd522a0fe17f331495
Noah Jutz <noah@noahjutz.com>
2025-03-15 20:15:17 +0100

    Use material3 in WorkoutCompleted

--- app/src/main/java/com/noahjutz/gymroutines/ui/workout/completed/WorkoutCompleted.kt
+++ app/src/main/java/com/noahjutz/gymroutines/ui/workout/completed/WorkoutCompleted.kt
@@ -1,25 +1,35 @@
 package com.noahjutz.gymroutines.ui.workout.completed
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Undo
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.noahjutz.gymroutines.R
 import org.koin.androidx.compose.getViewModel
 import org.koin.core.parameter.parametersOf
 
-@ExperimentalMaterialApi
 @Composable
 fun WorkoutCompleted(
     workoutId: Int,
@@ -48,8 +58,14 @@ fun WorkoutCompleted(
         Spacer(Modifier.height(80.dp))
         Column(Modifier.fillMaxWidth()) {
             Column(Modifier.padding(horizontal = 16.dp)) {
-                Text(stringResource(R.string.title_workout_completed), style = typography.h2)
-                Text(stringResource(R.string.body_workout_completed), style = typography.h5)
+                Text(
+                    stringResource(R.string.title_workout_completed),
+                    style = typography.headlineMedium
+                )
+                Text(
+                    stringResource(R.string.body_workout_completed),
+                    style = typography.headlineSmall
+                )
             }
             val isUpdateRoutineChecked by viewModel.isUpdateRoutineChecked.collectAsState(
                 initial = false
@@ -82,22 +98,6 @@ fun WorkoutCompleted(
             shape = RoundedCornerShape(percent = 100)
         ) {
             Text(stringResource(R.string.btn_close))
-        }
-    }
-}
-
-@ExperimentalMaterialApi
-@Composable
-@Preview
-fun WorkoutCompletedPreview() {
-    MaterialTheme {
-        Surface {
-            WorkoutCompleted(
-                workoutId = 0,
-                routineId = 0,
-                popBackStack = { },
-                navToWorkoutInProgress = { }
-            )
         }
     }
 }
